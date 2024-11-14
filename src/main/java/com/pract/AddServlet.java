868d8b5d0146b59ca26e73576598c8c1f74ad077
AnushaSV-6 <anushasv02@gmail.com>
2024-11-14 12:41:46 +0530

    http session cookie

--- src/main/java/com/pract/AddServlet.java
+++ src/main/java/com/pract/AddServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class AddServlet extends HttpServlet {
     
@@ -23,10 +24,21 @@ public class AddServlet extends HttpServlet {
         int k = i + j;
         k= k*k;
         
-        req.setAttribute("k", k);
+        HttpSession session =req.getSession();
+        session.setAttribute("k", k);
+        
+        res.sendRedirect("sq");
+        //req.setAttribute("k", k);
         
         
-         RequestDispatcher rd= req.getRequestDispatcher("sq");
-      rd.forward(req, res);
-    }
-}
+        // RequestDispatcher rd= req.getRequestDispatcher("sq");
+        // rd.forward(req, res);
+         }
+     }
+
+//RequestDispatcher is an interface used to forward or include content from another resource (like another servlet or JSP).
+// req.getRequestDispatcher("sq") creates a RequestDispatcher object that points to a resource named "sq". This is likely a servlet or JSP path within the web application.
+  
+
+
+
--- src/main/java/com/pract/SqServlet.java
+++ src/main/java/com/pract/SqServlet.java
@@ -6,15 +6,19 @@ import java.io.PrintWriter;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class SqServlet extends HttpServlet {
 	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException{
 		
 		
-		int k=(int) req.getAttribute("k");
+		//int k=(int) req.getAttribute("k");
+//This calls the getAttribute method on the HttpServletRequest object req.
+//"k" is the name of the attribute that was set in the request, presumably by another servlet or JSP (e.g., req.setAttribute("k", k);).
+//getAttribute("k") retrieves the value associated with "k" from the request object.
 		
-		
-		k=k*k;
+		HttpSession session= req.getSession();
+	int k=(int) session.getAttribute("k");
 		
 		PrintWriter out=res.getWriter();
 		out.println("result of Square is:"+k);
